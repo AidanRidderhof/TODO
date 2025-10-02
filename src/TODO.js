4b26fdd277aa5d3ddcd1c20e424088dcc67c53a1
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-02 14:26:43 -0700

    html skelton and todo.js

---
+++ src/TODO.js
@@ -0,0 +1,7 @@
+export const TODO = function(title, description, duedate, priority, notes) {
+    this.title = title
+    this.description = description
+    this.duedate = duedate
+    this.priority = priority
+    this.notes = notes
+}
