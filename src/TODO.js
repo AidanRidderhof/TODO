f4977306b9db5284573cb9bd53b65aa746399051
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-02 23:54:32 -0700

    Etablish ARMproto as a prototype for Project and ProjectManager

--- src/TODO.js
+++ src/TODO.js
@@ -1,15 +1,3 @@
-export function Project(name) {
-    this.name = name
-    this.tasks = []
-    this.id = null
-
-    this.addTODO = function(title, description, duedate, priority, notes) {
-        const task = new TODO(title, description, duedate, priority, notes)
-        this.tasks.push(task)
-    }
-
-}
-
 export function TODO(title, description, duedate, priority, notes) {
     this.title = title
     this.description = description
@@ -20,45 +8,50 @@ export function TODO(title, description, duedate, priority, notes) {
     this.id = null
 }
 
+export function Project(name) {
+    this.name = name
+    this.repo = []
+    this.id = null
+}
+
 //refactor names to work with armproto
 
 export function ProjectManager() {
-    this.projects = []
+    this.repo = []
+}
 
-    this.addProject = function(name) {
-        const project = new Project(name)
-        this.projects.push(project)
+const ARMproto = {
+    addItem: function(object, ...args) {
+        const entry = new object(...args)
+        this.repo.push(entry)
         this.mapIDS()
-    }
+        return entry
+    },
 
-    this.removeProject = function(id) {
+    removeItem: function(id) {
         const index = id-1
-        this.projects.splice(index, 1)
+        this.repo.splice(index, 1)
         this.mapIDS()
-    }
+    },
 
-    this.mapIDS = function () {
-        this.projects.forEach((project, i) => {
-            project.id = i + 1
+    mapIDS: function() {
+        this.repo.forEach((item, i) => {
+            item.id = i + 1
         })
     }
 }
 
-/* const ARMproto = {
-    addItem: function(object, ...args) {
-        const entry = new object(...args)
-        this.repo.push(entry)
-        mapIDS()
-    }
+Project.prototype = Object.create(ARMproto)
+Project.prototype.constructor = Project
 
-    removeItem: function(id) {
-        index = id-1
-        this.projects = this.projects.splice(index, 1)
-        mapIDS()
-    },
-    mapIDS: function() {
-        this.projects.map((project, i) => {
-            project.id = i + 1
-        })
-    }
-} */
+ProjectManager.prototype = Object.create(ARMproto)
+ProjectManager.prototype.constructor = Project
+
+
+Project.prototype.addTODO = function(...args) {
+  return this.addItem(TODO, ...args)
+}
+
+ProjectManager.prototype.addProject = function(name) {
+  return this.addItem(Project, name)
+}
