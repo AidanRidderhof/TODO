9fd2f66d0427833e2717f6a30d704549d52257e5
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-02 21:00:02 -0700

    took WWAAAAYYYY too long to get the submit on new project working

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import "./style.css";
 const new_proj_button = document.querySelector("#new-proj")
 const proj_dialogue = document.querySelector("dialog")
 const cancel_proj = document.querySelector("#cancel-proj")
-const submit_proj = document.querySelector("submit-proj")
+const submit_proj = document.querySelector("#submit-proj")
+const new_proj_form = document.querySelector("#new-proj-form")
 
+const Test = new Project("I am a test project")
+console.log(Test.name)
+Test.addTODO("exname", "exdes", "exdd", "exprior", "exnotes")
+console.log(Test.tasks[0])
+
+//reveals dialogue form
 new_proj_button.addEventListener("click", () => {
     proj_dialogue.showModal();
 });
 
-cancel_proj.addEventListener("click",() => {
+//closes dialogue
+cancel_proj.addEventListener("click", () => {
     proj_dialogue.close()
-})
+});
+
+//submits text and closes WIP
+submit_proj.addEventListener("click", () => {
+    const projectName = document.querySelector("#proj-title-input")
+    const task = new Project(projectName.value)
+    console.log(task.name)
+    projectName.value = ""
+    proj_dialogue.close()
+});
