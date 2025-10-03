e3e6b971e5ec809878eb061ff03c99cc99ae9595
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-02 21:33:42 -0700

    new object constructor ProjectManager contains array of projects

--- src/TODO.js
+++ src/TODO.js
@@ -2,7 +2,6 @@ export function Project(name) {
     this.name = name
     this.tasks = []
 
-    // expose addTODO as an instance method so `this` points to the project
     this.addTODO = function(title, description, duedate, priority, notes) {
         const task = new TODO(title, description, duedate, priority, notes)
         this.tasks.push(task)
@@ -17,4 +16,12 @@ export function TODO(title, description, duedate, priority, notes) {
     this.priority = priority
     this.notes = notes
     this.isComplete = false
+}
+
+export function ProjectManager() {
+    this.projects = []
+    this.addProject = function(name) {
+        const project = new Project(name)
+        this.projects.push(project)
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Project, TODO } from './TODO.js'
+import { ProjectManager, Project, TODO } from './TODO.js'
 import "./style.css";
 
 const new_proj_button = document.querySelector("#new-proj")
@@ -6,7 +6,8 @@ const proj_dialogue = document.querySelector("dialog")
 const cancel_proj = document.querySelector("#cancel-proj")
 const submit_proj = document.querySelector("#submit-proj")
 const new_proj_form = document.querySelector("#new-proj-form")
-const project_repo = []
+
+const project_repo = new ProjectManager()
 
 //reveals dialogue form
 new_proj_button.addEventListener("click", () => {
@@ -21,9 +22,8 @@ cancel_proj.addEventListener("click", () => {
 //submits text and closes 
 submit_proj.addEventListener("click", () => {
     const projectName = document.querySelector("#proj-title-input")
-    const task = new Project(projectName.value)
-    project_repo.push(task)
-    console.log(project_repo[0])
+    project_repo.addProject(projectName.value)
+    console.log(project_repo.projects[0])
     projectName.value = ""
     proj_dialogue.close()
 });
