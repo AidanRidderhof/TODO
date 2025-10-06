ff42cf8434324677389e7b441e2f05c8be417ecc
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-06 15:12:56 -0700

    for each iterates over projects

--- src/getStorage.js
+++ src/getStorage.js
@@ -3,6 +3,12 @@ import { ProjectManager, Project, TODO } from './TODO.js'
 export function getLocalStorage() {
     const storedObj = localStorage.getItem('projectmanager') ? localStorage.getItem('projectmanager') : new ProjectManager
     const projectManager = JSON.parse(storedObj)
-    console.log(projectManager)
+    domDisplay(projectManager)
     return(projectManager)
+}
+
+function domDisplay(projectManager) {
+    for (let p_index in projectManager.repo) {
+        console.log(projectManager.repo[p_index])
+    }
 }
