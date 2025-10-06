81a066524f32a2dd2db195e146705c01e533e0a1
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-05 22:42:55 -0700

    remove comments

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ import "./style.css";
 function DOMmanipulator() {
     const projectManager = new ProjectManager()
 
-    /*const proj1 = projectManager.addProject("proj1")
-    const proj2 = projectManager.addProject("proj2")
-    const proj3 = projectManager.addProject("proj3")
-
-    proj1.addTODO("Dummy1", "Dummy2", "Dummy3", "Dummy4", "Dummy5")
-    proj2.addTODO("stupid1", "stupid2", "stupid3", "stupid4", "stupid5")
-    proj3.addTODO("notreal1", "notreal2", "notreal3", "notreal4", "notreal5")
-
-    const btn1 = document.createElement("button")
-    const btn2 = document.createElement("button")
-    const btn3 = document.createElement("button")
-
-    btn1.classList.add("button")*/
-
-
-
-
-
     const new_proj_button = document.querySelector("#new-proj")
     const proj_dialogue = document.querySelector("#proj-modal")
     const cancel_proj = document.querySelector("#cancel-proj")
@@ -83,7 +65,6 @@ function DOMmanipulator() {
 
     new_task_form.addEventListener("submit", submitTaskForm)
 
-    //WIP
     function submitTaskForm(event) {
         event.preventDefault()
         const new_task_title = document.querySelector("#task-title")
@@ -94,6 +75,9 @@ function DOMmanipulator() {
         const container = document.querySelectorAll('.content.live')
 
         const task = new TODO(new_task_title.value, new_task_description.value, new_task_duedate.value, priority.value, notes.value)
+        const proj_id = container.dataset.id.charAt(container.dataset.id-1)
+
+        console.log(proj_id.charAt(proj_id-1))
 
         const task_div = document.createElement("div")
         task_div.innerText = task.description
